cd53af03d194fffe71fec6d98897ba17412a75d7
sgbalogh <sgbalogh@gmail.com>
2015-10-05 16:54:09 -0400

    removes old (commented) code, rearranges elements, tweaks styling

--- themes/emiglio/neatline/exhibits/themes/citations/show.php
+++ themes/emiglio/neatline/exhibits/themes/citations/show.php
@@ -16,34 +16,6 @@
 <?php echo nl_getExhibitMarkup(); ?>
 <?php echo nl_getNarrativeMarkup(); ?>
 
-<!-- <div id="faceting">
-      <div class="AdditionalCareers">Additional Careers</div>
-  	  <div class="AntiSemiticRhetoric">Anti-Semitic Rhetoric</div>
-  	  <div class="Bartering">Bartering</div>
-  	  <div class="BusinessPractices">Business Practices</div>
-  	  <div class="ConflictViolenceCommunal">Conflict/Violence -- Communal</div>
-  	  <div class="ConflictViolencePersonal">Conflict/Violence -- Personal</div>
-  	  <div class="ContextualResource">Contextual Resource</div>
-  	  <div class="CulturalExchange">Cultural Exchange</div>
-  	  <div class="Family">Family</div>
-  	  <div class="LiteraryArtisticDepiction">Literary or Artistic Depiction</div>
-  	  <div class="NonPeddlingBackground">Non-Peddling Background</div>
-  	  <div class="PeddlingBackground">Peddling Background</div>
-  	  <div class="ReligiousPractices">Religious Practices</div>
-  	  <div class="RestrictionsCommerce">Restrictions on Trade/Commerce</div>
-  	  <div class="Routes">Routes</div>
-  	  <div class="Sponsor">Sponsor</div>
-  	  <div class="SubsequentCommercialCareers">Subsequent Commercial Careers</div>
-  	  <div class="SubsequentNonCommercial">Subsequent Non-Commercial Careers</div>
-  	  <div class="Transportation">Transportation</div>
-  	  <div class="VictimsOfCrime">Victims Of Crime</div>
-  	  <div class="Wares">Wares</div>
-<br>
-	<div class="reset">Reset All Facets</div>
-<br>
-<div class="Book">Book</div>
-  	  <div class="JournalArticle">Journal Article</div>
-</div>-->
 <div id="multi">
 <div class="checkbox">
   <label>
@@ -127,6 +99,9 @@
   </label>
    </div><div class="checkbox" data-toggle="buttons">
 
+</div>
+</div>
+
 <select name="selector" class="form-control">
 <option name="tag" selected="selected" disabled="disabled">Search by Item Type</option>
 <option name="tag" value="Book">Book</option>
@@ -139,12 +114,6 @@
 <option name="tag" value="All">All</option>
 </select>
 
-     </div><div class="checkbox" data-toggle="buttons">
-   <label class="btn btn-success btn-xs">
-    <input type="checkbox" name="tag" value="BermanPopulation"> Berman Center Jewish Population 2010
-  </label>
-</div>
-</div>
 <br>
 <button type="submit" name="submit" class="btn btn-default">Apply Selected Facets</button>
 <button type="submit" name="disabletimeline" id="disable" class="btn btn-default">Disable Timeline Faceting</button>
