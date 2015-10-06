accc23d808ed63b4c2ed598d38026abb3e0955b6
sgbalogh <sgbalogh@gmail.com>
2015-10-06 11:52:10 -0400

    rearranges element fields

--- themes/emiglio/neatline/exhibits/themes/citations/item.php
+++ themes/emiglio/neatline/exhibits/themes/citations/item.php
@@ -100,11 +100,6 @@
 
 
 
-    <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
-
-</div><!-- end primary -->
-
-
     <!-- The following returns all of the files associated with an item. -->
     <?php if ((get_theme_option('Item FileGallery') == 1) && metadata($item, 'has files')): ?>
     <div id="itemfiles" class="element">
@@ -113,21 +108,19 @@
     </div>
     <?php endif; ?>
     
-
-
-    <!-- The following prints a list of all tags associated with the item -->
-    <?php if (metadata($item, 'has tags')): ?>
-
-        <h2>Topics</h2>
-        <div class="element-text tags"><?php echo tag_string('item'); ?></div>
-
-    <?php endif; ?>
-
 <?php if (metadata('item', array('Dublin Core', 'Subject'))): ?>
 <h2>Individuals</h2>
 <div class="element-text tags"><?php echo metadata('item', array('Dublin Core', 'Subject'), array('delimiter' => '; ')); ?></div>
 
 <?php endif; ?>
+
+
+    <!-- The following prints a list of all tags associated with the item -->
+    <?php if (metadata($item, 'has tags')): ?>
+        <h2>Topics</h2>
+        <div class="element-text tags"><?php echo tag_string('item'); ?></div>
+    <?php endif; ?>
+
 
     <!-- If the item belongs to a collection, the following creates a link to that collection. -->
     <?php if (get_collection_for_item()): ?>
